64706cc8f712bc22849be8680b0a750757c6a6a5
Alberto Hanna <alberto.adell17@gmail.com>
2022-05-23 21:55:11 +0200

    Finished Day11 P1

--- Day11.py
+++ Day11.py
@@ -1,9 +1,6 @@
 from Day0 import day_data
 
-data_in = day_data(11).splitlines()
-
-
-def create_matrix(data_in: list) -> dict:
+def create_matrix(data_in: list) -> tuple[dict, int, int]:
     ii = len(data_in)
     jj = len(data_in[0])
 
@@ -13,13 +10,18 @@ def create_matrix(data_in: list) -> dict:
         for j in range(jj)
     }
     
-    return matrix
+    return matrix, ii, jj
 
 
-def step(matrix: dict) -> dict:
+def step(matrix: dict) -> tuple[dict, list]:
+    flashed = []
     for k, v in matrix.items():
-        matrix[k] = v + 1
-    return matrix
+        if v == 9:
+            matrix[k] = 0
+            flashed.append(k)
+        else:
+            matrix[k] = v + 1
+    return matrix, flashed
 
 
 def get_neighbors(point: tuple[int, int], matrix: dict) -> list[tuple[int, int]]:
@@ -35,12 +37,46 @@ def get_neighbors(point: tuple[int, int], matrix: dict) -> list[tuple[int, int]]
     
     return neighbors
 
+def double_step(matrix:dict, flashed:list):
+    c_f = len(flashed)
+    
+    while flashed:
+        p = flashed.pop()
+        for n_p in get_neighbors(p, matrix):
+            if matrix[n_p] == 0:
+                continue
+            
+            elif matrix[n_p] == 9:
+                matrix[n_p] = 0
+                flashed.append(n_p)
+                c_f += 1
+            
+            else:
+                matrix[n_p] += 1
+    
+    return matrix, c_f
 
-def print_matrix(matrix: dict, ii: int, jj: int) -> None:
+def triple_step(matrix, n=1):
+    t_f = 0
+    for _ in range(n):
+        matrix, flashed = step(matrix)
+        matrix, c_f = double_step(matrix, flashed)
+        t_f += c_f
+    return matrix, t_f
+
+def print_matrix(matrix: dict, ii, jj) -> None:
     for i in range(ii):
         for j in range(jj):
             print(matrix[(i, j)], end='')
         print()
 
 # print_matrix(step(matrix), ii, jj)
+
+data_in = day_data(11).splitlines()
+matrix, ii, jj = create_matrix(data_in)
 # print(get_neighbors((0, 0), matrix))
+
+matrix, t_f = triple_step(matrix, 100)
+
+print('\nNumber of Flashed:', t_f, '\n')
+print_matrix(matrix, ii, jj)
