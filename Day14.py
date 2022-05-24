676c2bd141ff12ec1ce06b343260a96688bf9a54
Alberto Hanna <alberto.adell17@gmail.com>
2022-05-25 00:59:46 +0200

    * Day14.py ** Ugly code & need tuning

---
+++ Day14.py
@@ -0,0 +1,30 @@
+from Day0 import day_data
+from collections import Counter
+from copy import deepcopy
+
+temp, map_ = day_data(14).split('\n\n')
+
+mapper = {
+    k: v
+    for k_v in map_.strip().split('\n')
+    for k, v in [k_v.split(' -> ')]
+}
+
+c = Counter(map(''.join, zip(temp, temp[1:])))
+
+for i in range(40):
+    nc = {}
+    for k, v in c.items():
+        nc[k[0] + mapper[k]] = nc.get(k[0] + mapper[k], 0) + v
+        nc[mapper[k] + k[1]] = nc.get(mapper[k] + k[1], 0) + v
+
+    c = deepcopy(nc)
+
+    if i not in (9, 39):
+        continue
+
+    nc = {}
+    for (k1, k2), v in c.items():
+        nc[k1] = nc.get(k1, 0) + v
+
+    print((max(nc.values()) - min(nc.values()))-1)
